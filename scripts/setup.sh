cabc6328a146aabe7c53dd8043077427d6fcd549
frothbeast <frothbeast@gmail.com>
2026-01-27 12:10:00 -0500

    added PIC code and removed secure information to config.h
    added comments in setup.sh to guide me when I am daft
    
    Signed-off-by: frothbeast <frothbeast@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,20 @@
+# use USB stick to install ubuntu DO NOT USE GIT SSH KEY UNLESS YOU LIKE FAILING
+# find IP of machine I used static ip <wired_IP_address> you can change this in the first file in /etc/netplan
+# network:
+#  version: 2
+#  ethernets:
+#    eno1:
+#      dhcp4: false
+#      addresses:
+#        - <wired_IP_address>/24
+#      routes:
+#        - to: default
+#          via: <wired_IP_gateway>
+#      nameservers:
+#        addresses: [8.8.8.8, 1.1.1.1]
+# log in with laptop from local LAN
+# from a local LAN computer, go to users/frothbeast/.ssh/known_hosts/  get rid of all lines(keys) with this IP address
+
 sudo apt update
 sudo apt upgrade
 
