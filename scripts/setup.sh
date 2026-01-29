077b9fd4918574aef1d98579eea71556241bdedb
Frothbeast <frothbeast@gmail.com>
2026-01-28 21:53:13 -0500

    added more stuff to try to get the script to install the server
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,15 @@
 
 # 2. Run the database setup script
 sudo chmod +x setup_db.sh
-.setup_db.sh
+./setup_db.sh
 
 # 3. Setup Python Environment
 sudo apt install -y python3-venv
 pwd
 echo "sudo apt install -y python3-venv"
-cd server
+cd ../server
 pwd
-echo "cd server"
+echo "cd ../server"
 
 python3 -m venv venv
 pwd
@@ -19,30 +19,32 @@ source venv/bin/activate
 pwd
 echo "source venv/bin/activate"
 
-pip install -r ../server/requirements.txt
+pip install -r requirements.txt
 pwd
-echo "pip install -r ../server/requirements.txt"
-
+echo "pip install -r requirements.txt"
+sudo rm -f /usr/local/bin/pm2
+sudo rm -rf /usr/local/lib/node_modules/pm2
+sudo rm -rf /usr/local/lib/node_modules/.pm2*
 cd ../client/
 pwd
 echo "sd ../client/"
+sudo rm -rf /usr/local/lib/node_modules/pm2
 
 # Setup Node/React
 sudo apt install -y npm
 
 pwd
 echo "sudo apt install -y npm"
+
 sudo npm install -g pm2
 pwd
+npm install
 echo "npm install -g pm2"
 npm run build
 
 pwd
 echo "npm run build"
-cd server
 
-pwd
-echo "cd server"
 pm2 start venv/bin/python --name "iot-collector" -- pythonDataCollector.py
 pm2 startup | tail -n 1 | bash
 pm2 save
