c3fa9658897ba7740833568b004c7eae96f64994
frothbeast <frothbeast@gmail.com>
2026-02-03 16:28:58 -0500

    css changes

--- client/src/components/sumpTable/sumpTable.js
+++ client/src/components/sumpTable/sumpTable.js
@@ -3,32 +3,34 @@ import './sumpTable.css';
 
 const SumpTable = ({ sumpRecords = [] }) => {
     return (
-        <table className="sump-table">
-            <thead className="sump-table__head">
-                <tr className="sump-table__row">
-                    <th className="sump-table__header-cell">DATE/TIME</th>
-                    <th className="sump-table__header-cell">High ADC</th>
-                    <th className="sump-table__header-cell">Low ADC</th>
-                    <th className="sump-table__header-cell">On Time</th>
-                    <th className="sump-table__header-cell">Off Time</th>
-                    <th className="sump-table__header-cell">Hours ON</th>
-		    <th className="sump-table__header-cell">Duty Cycle</th> 
-               </tr>
-            </thead>
-            <tbody>
-                {Array.isArray(sumpRecords) && sumpRecords.map((record) => (
-                    <tr key={record.id} className="sump-table__row">
-                        <td className="sump-table__cell">{record.payload?.datetime ?? "N/a"}</td>
-                        <td className="sump-table__cell">{record.payload?.Hadc ?? "N/a"}</td>
-                        <td className="sump-table__cell">{record.payload?.Ladc ?? "N/a"}</td>
-                        <td className="sump-table__cell">{record.payload?.timeOn ?? "N/a"}</td>
-                        <td className="sump-table__cell">{record.payload?.timeOff ?? "N/a"}</td>
-                        <td className="sump-table__cell">{record.payload?.hoursOn ?? "N/a"}</td>
-			<td className="sump-table__cell">{record.payload?.duty ?? "N/a"}</td>
+	<div classname="sumpTableContainer">
+            <table className="sump-table">
+                <thead className="sump-table__head">
+                    <tr className="sump-table__row">
+                        <th className="sump-table__header-cell">DATE/TIME</th>
+                        <th className="sump-table__header-cell">High ADC</th>
+                        <th className="sump-table__header-cell">Low ADC</th>
+                        <th className="sump-table__header-cell">On Time</th>
+                        <th className="sump-table__header-cell">Off Time</th>
+                        <th className="sump-table__header-cell">Hours ON</th>
+    		        <th className="sump-table__header-cell">Duty Cycle</th> 
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {Array.isArray(sumpRecords) && sumpRecords.map((record) => (
+                        <tr key={record.id} className="sump-table__row">
+                            <td className="sump-table__cell">{record.payload?.datetime ?? "N/a"}</td>
+                            <td className="sump-table__cell">{record.payload?.Hadc ?? "N/a"}</td>
+                            <td className="sump-table__cell">{record.payload?.Ladc ?? "N/a"}</td>
+                            <td className="sump-table__cell">{record.payload?.timeOn ?? "N/a"}</td>
+                            <td className="sump-table__cell">{record.payload?.timeOff ?? "N/a"}</td>
+                            <td className="sump-table__cell">{record.payload?.hoursOn ?? "N/a"}</td>
+			    <td className="sump-table__cell">{record.payload?.duty ?? "N/a"}</td>
+                        </tr>
+                    ))}
+                </tbody>
+	    </table>
+	</div>
     );
 };
 
