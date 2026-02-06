3d512850dc0d1542a689ba9c86f70bdbec96cb68
Frothbeast <frothbeast@gmail.com>
2026-02-06 17:22:13 -0500

    This will remove the negative number
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -44,9 +44,9 @@ function App() {
             timeOff: { avg: StatsLib.avg(timeOffs), max: StatsLib.max(timeOffs), min: StatsLib.min(timeOffs) },
             hoursOn: { avg: StatsLib.avg(hoursOns), max: StatsLib.max(hoursOns), min: StatsLib.min(hoursOns) },
             duty: { avg: StatsLib.avg(duties), max: StatsLib.max(duties), min: StatsLib.min(duties) },
-            datetime: {     avg: StatsLib.avg(datetime.map(s => new Date(s).getTime()).slice(1).map((v, i, arr) => v - new Date(datetime[i]).getTime())),
-                            max: StatsLib.max(datetime.map(s => new Date(s).getTime()).slice(1).map((v, i, arr) => v - new Date(datetime[i]).getTime())),
-                            min: StatsLib.min(datetime.map(s => new Date(s).getTime()).slice(1).map((v, i, arr) => v - new Date(datetime[i]).getTime()))
+            datetime: {     avg: StatsLib.avg(datetime.slice(1).map((v, i) => new Date(datetime[i]).getTime() - new Date(v).getTime())),
+                            max: StatsLib.max(datetime.slice(1).map((v, i) => new Date(datetime[i]).getTime() - new Date(v).getTime())),
+                            min: StatsLib.min(datetime.slice(1).map((v, i) => new Date(datetime[i]).getTime() - new Date(v).getTime()))
                         },
             lastDatetime: lastDatetime
         };
