602b06d6418a3a3b83e9c32b9549622496117a1b
Frothbeast <frothbeast@gmail.com>
2026-02-07 17:12:01 -0500

    overlay legend
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/components/sumpChart.js
+++ client/src/components/sumpChart.js
@@ -27,22 +27,37 @@ const SumpChart = ({ datasets, labels }) => {
         responsive: true,
         maintainAspectRatio: false,
         plugins: { 
-          legend: { display: true, 
-                    labels: {
-                      boxWidth: 10,
-                      boxHeight: 2,
-                      padding: 1,
-                      font: {size: 6}
-
-                    }
+          legend: { 
+            display: true,
+            position: 'top', 
+            align: 'start',   
+            labels: {
+              boxWidth: 10,
+              boxHeight: 2,
+              padding: 1,
+              font: {size: 10}
+            }
           } 
+         },
+        layout: {
+          padding: {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+          }
         },
+
         scales: {
           x: { display: false },
-          y: { display: false }
+          y: { 
+            display: false,
+            grace: '10%'
+          }
         }
       }
-    });
+    }
+  );
 
     return () => chartInstance.current?.destroy();
   }, [datasets, labels]);
