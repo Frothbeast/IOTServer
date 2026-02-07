ef6527462cb1a4389977c8a76ce6491c7d8a93ca
Frothbeast <frothbeast@gmail.com>
2026-02-07 16:22:53 -0500

    added forgotten sumpChart.js
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

---
+++ client/src/components/sumpChart.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const SumpChart = ({ data, label, color }) => {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    const ctx = chartRef.current.getContext('2d');
+    if (chartInstance.current) chartInstance.current.destroy();
+
+    chartInstance.current = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: data.map((_, i) => i),
+        datasets: [{
+          label: label,
+          data: data,
+          borderColor: color,
+          tension: 0.1,
+          pointRadius: 0
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: { legend: { display: false } },
+        scales: { x: { display: false }, y: { display: false } }
+      }
+    });
+
+    return () => chartInstance.current?.destroy();
+  }, [data, label, color]);
+
+  return <canvas ref={chartRef} />;
+};
+
+export default SumpChart;
