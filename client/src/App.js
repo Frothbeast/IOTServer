55cc32b95c08a58ec9e59ce3061b6b0a2d8bd3ce
Frothbeast <frothbeast@gmail.com>
2026-02-07 18:29:06 -0500

    added graphs to sidebar
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,12 @@ function App() {
       <main>
         <div className="tableWrapper">
           <SumpTable sumpRecords={sumpRecords} columnStats={columnStats} />
+          <div className="sidebar">
+            <div class="chartContainer1">Chart 1</div>
+            <div class="chartContainer2">Chart 2</div>
+            <div class="chartContainer3">Chart 3</div>
+            <div class="chartContainer4">Chart 4</div>
+          </div>
         </div>
       </main>
     </div>
