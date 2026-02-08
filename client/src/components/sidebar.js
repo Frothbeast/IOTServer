9e436d1a7eb8e3f9117cdda4342a4582e0f90be8
Frothbeast <frothbeast@gmail.com>
2026-02-08 12:56:37 -0500

    added specific <div className="sidebar-content"> to contain and help define size for cqw
    Signed-off-by: Frothbeast <frothbeast@gmail.com>

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -32,43 +32,45 @@ const Sidebar = ({ isOpen, sumpRecords }) => {
 
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <div className="chartContainer1">
-        <SumpChart 
-          labels={sumpRecords.map((_, i) => i)}
-          datasets={[{ label: "Ladc", color: "pink", data: sumpRecords.map(r => r.payload?.Ladc) },
-                     { label: "Hadc", color: "lightgreen", data: sumpRecords.map(r => r.payload?.Hadc) }]} 
-          options={sidebarChartOptions}           
-        />
-      </div>
-      <div className="chartContainer2">
-        <SumpChart 
-          labels={sumpRecords.map((_, i) => i)}
-          datasets={[{ label: "timeOn", color: "yellow", data: sumpRecords.map(r => r.payload?.timeOn) },
-                     { label: "timeOff", color: "red", data: sumpRecords.map(r => r.payload?.timeOff) }]} 
-          options={sidebarChartOptions}
-        />
-      </div>
-      <div className="chartContainer3">
-        <SumpChart 
-          labels={sumpRecords.map((_, i) => i)}
-          datasets={[{ label: "duty", color: "lightgreen", data: sumpRecords.map(r => r.payload?.duty) }]}
-          options={sidebarChartOptions} 
-        />
-      </div>
-      <div className="chartContainer4">
-        <SumpChart
-          labels={sumpRecords.map((_, i) => i)}
-          datasets={[{
-            label: "period",color: "red",
-            data: sumpRecords.slice(1).map((r, i) => {
-              const current = new Date(r.payload?.datetime).getTime();
-              const previous = new Date(sumpRecords[i].payload?.datetime).getTime();
-              return ( previous -current) / 60000;
-            })
-            }
-          ]}
-          options={sidebarChartOptions}
-        />
+      <div className="sidebar-content">>
+        <div className="chartContainer1">
+          <SumpChart 
+            labels={sumpRecords.map((_, i) => i)}
+            datasets={[{ label: "Ladc", color: "pink", data: sumpRecords.map(r => r.payload?.Ladc) },
+                      { label: "Hadc", color: "lightgreen", data: sumpRecords.map(r => r.payload?.Hadc) }]} 
+            options={sidebarChartOptions}           
+          />
+        </div>
+        <div className="chartContainer2">
+          <SumpChart 
+            labels={sumpRecords.map((_, i) => i)}
+            datasets={[{ label: "timeOn", color: "yellow", data: sumpRecords.map(r => r.payload?.timeOn) },
+                      { label: "timeOff", color: "red", data: sumpRecords.map(r => r.payload?.timeOff) }]} 
+            options={sidebarChartOptions}
+          />
+        </div>
+        <div className="chartContainer3">
+          <SumpChart 
+            labels={sumpRecords.map((_, i) => i)}
+            datasets={[{ label: "duty", color: "lightgreen", data: sumpRecords.map(r => r.payload?.duty) }]}
+            options={sidebarChartOptions} 
+          />
+        </div>
+        <div className="chartContainer4">
+          <SumpChart
+            labels={sumpRecords.map((_, i) => i)}
+            datasets={[{
+              label: "period",color: "red",
+              data: sumpRecords.slice(1).map((r, i) => {
+                const current = new Date(r.payload?.datetime).getTime();
+                const previous = new Date(sumpRecords[i].payload?.datetime).getTime();
+                return ( previous -current) / 60000;
+              })
+              }
+            ]}
+            options={sidebarChartOptions}
+          />
+        </div>
       </div>
     </div>
   );
