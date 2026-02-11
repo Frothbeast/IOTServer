cf7323b8e53fbb59fdcdf4db0a9bd4599dd5f90b
frothbeast <frothbeast@gmail.com>
2026-02-11 08:59:16 -0500

    added sections to the js to contain the 3 main sections of the controlbar header
    css fixes to follow
    
    Signed-off-by: frothbeast <frothbeast@gmail.com>

--- client/src/components/ControlBar/ControlBar.js
+++ client/src/components/ControlBar/ControlBar.js
@@ -34,85 +34,91 @@ const ControlBar = ({ selectedHours, onHoursChange, columnStats, sumpRecords, to
   return (
     <header className="controlBar">
       <div className="brand">Sump</div>
-      <select className="selectedHours" value={selectedHours} onChange={(e) => onHoursChange(Number(e.target.value))}>
-        <option value={1}>Hour</option>
-        <option value={24}>Day</option>
-        <option value={168}>Week</option>
-      </select>
-        
-      <div className="chartContainer1">
-        <div className="chart-watermark">ADC</div>
-        <SumpChart 
-          labels={sumpRecords.map((_, i) => i)}
-            datasets={[
-              { 
-                label: "Ladc", 
-                color: "pink", 
-                data: sumpRecords.map(r => r.payload?.Ladc),
-              },
-             { 
-                label: "Hadc", 
-                color: "green", 
-                data: sumpRecords.map(r => r.payload?.Hadc),
-              }
-            ]}
-            options={getOptions(400, 1024)}  
-          />
+      <div className="centerSection">
+        <select className="selectedHours" value={selectedHours} onChange={(e) => onHoursChange(Number(e.target.value))}>
+          <option value={1}>Hour</option>
+          <option value={24}>Day</option>
+          <option value={168}>Week</option>
+        </select>
+        <div className="buttonRow">
+            <div className="lastRun">Last Run: {columnStats?.lastDatetime ?? "N/a"}</div>
+            <button className="sidebarButton" onClick={toggleSidebar}>
+                {isSidebarOpen ? "Close Chart" : "View Graph"}
+            </button>
         </div>
+      </div>
+      <div className="chartSection">
+        <div className="chartContainer1">
+          <div className="chart-watermark">ADC</div>
+            <SumpChart
+              labels={sumpRecords.map((_, i) => i)}
+                datasets={[
+                  {
+                    label: "Ladc",
+                    color: "pink",
+                    data: sumpRecords.map(r => r.payload?.Ladc),
+                  },
+                 {
+                    label: "Hadc",
+                    color: "green",
+                    data: sumpRecords.map(r => r.payload?.Hadc),
+                  }
+                ]}
+                options={getOptions(400, 1024)}
+              />
+          </div>
         <div className="chartContainer2">
           <div className="chart-watermark">TIME</div>
-          <SumpChart 
-            labels={sumpRecords.map((_, i) => i)}
-            datasets={[
-              { 
-                label: "timeOn", 
-                color: "yellow", 
-                data: sumpRecords.map(r => r.payload?.timeOn), 
-              },
-              { 
-                label: "timeOff", 
-                color: "red", 
-                data: sumpRecords.map(r => r.payload?.timeOff),  
-              } 
-            ]} 
-            options={getOptions(0, 1000)} 
-          />
+            <SumpChart
+              labels={sumpRecords.map((_, i) => i)}
+              datasets={[
+                {
+                  label: "timeOn",
+                  color: "yellow",
+                  data: sumpRecords.map(r => r.payload?.timeOn),
+                },
+                {
+                  label: "timeOff",
+                  color: "red",
+                  data: sumpRecords.map(r => r.payload?.timeOff),
+                }
+              ]}
+              options={getOptions(0, 1000)}
+            />
         </div>
         <div className="chartContainer3">
           <div className="chart-watermark">DUTY</div>
-          <SumpChart 
-            labels={sumpRecords.map((_, i) => i)}
-            datasets={[
-              { 
-                label: "duty", 
-                color: "green", 
-                data: sumpRecords.map(r => r.payload?.duty) 
-              } 
-            ]}
-            options={getOptions(0, 100)}  
-          />
+            <SumpChart
+              labels={sumpRecords.map((_, i) => i)}
+              datasets={[
+                {
+                  label: "duty",
+                  color: "green",
+                  data: sumpRecords.map(r => r.payload?.duty)
+                }
+              ]}
+              options={getOptions(0, 100)}
+            />
+          </div>
         </div>
         <div className="chartContainer4">
           <div className="chart-watermark">PERIOD</div>
-          <SumpChart 
-            labels={sumpRecords.map((_, i) => i)}
-            datasets={[{
-              label: "period",color: "red",
-              data: sumpRecords.slice(1).map((r, i) => {
-                const current = new Date(r.payload?.datetime).getTime();
-                const previous = new Date(sumpRecords[i].payload?.datetime).getTime();
-                return ( previous -current) / 60000;
-              })
-              }
-            ]}
-            options={getOptions(0, 100)}   
-          />
+            <SumpChart
+              labels={sumpRecords.map((_, i) => i)}
+              datasets={[{
+                label: "period",color: "red",
+                data: sumpRecords.slice(1).map((r, i) => {
+                  const current = new Date(r.payload?.datetime).getTime();
+                  const previous = new Date(sumpRecords[i].payload?.datetime).getTime();
+                  return ( previous -current) / 60000;
+                })
+                }
+              ]}
+              options={getOptions(0, 100)}
+            />
+          </div>
         </div>
-
-      <div className="lastRun">Last Run: {columnStats?.lastDatetime ?? "N/a"}</div>
-      <button className="sidebarButton" onClick={toggleSidebar}>
-        {isSidebarOpen ? "Close Chart" : "View Graph"}
-      </button>
+      </div>
     </header>
   );
 };
