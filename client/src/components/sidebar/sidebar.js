dca1f73680fc4c1e3c8a9cab1ef49c0c1167bba8
frothbeast <frothbeast@gmail.com>
2026-02-11 14:23:52 -0500

    missing path
    Signed-off-by: frothbeast <frothbeast@gmail.com>

--- client/src/components/sidebar/sidebar.js
+++ client/src/components/sidebar/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SumpChart from './sumpChart';
+import SumpChart from './sumpTable/sumpChart';
 import './sidebar.css'; 
 
 const Sidebar = ({ isOpen, sumpRecords }) => {
